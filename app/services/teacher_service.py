4060507c5852857ac5f1869c5a32c6471e9e1cb9
Halima Lena Camara <halimalenac@gmail.com>
2026-03-23 17:42:27 +0000

    Ajout de fonctionnalite - recherche et  modification

--- app/services/teacher_service.py
+++ app/services/teacher_service.py
@@ -1,29 +1,48 @@
 teachers = []
 
+
 def list_teachers():
     return teachers
 
-def add_teacher(name, email, speciality):
-    # Vérifier si l'email existe déjà
-    for t in teachers:
-        if t["email"] == email:
-            return None
-    
-    # Créer l'ID
-    if teachers:
-        new_id = teachers[-1]["id"] + 1
-    else:
-        new_id = 1
-    
-    teacher = {"id": new_id, "name": name, "email": email, "speciality": speciality}
-    teachers.append(teacher)
-    return teacher
 
 def get_teacher_by_id(teacher_id):
     for t in teachers:
         if t["id"] == teacher_id:
             return t
     return None
+
+
+def add_teacher(name, email, speciality):
+    for t in teachers:
+        if t["email"] == email:
+            return None
+    new_id = teachers[-1]["id"] + 1 if teachers else 1
+    teacher = {"id": new_id, "name": name, "email": email, "speciality": speciality}
+    teachers.append(teacher)
+    return teacher
+
+
+def update_teacher(teacher_id, name, email, speciality):
+    for t in teachers:
+        if t["id"] == teacher_id:
+            # Vérifie doublon email (sauf lui-même)
+            for other in teachers:
+                if other["email"] == email and other["id"] != teacher_id:
+                    return None
+            t["name"] = name
+            t["email"] = email
+            t["speciality"] = speciality
+            return t
+    return False
+
+
+def search_teachers(query):
+    query = query.strip().lower()
+    results = []
+    for t in teachers:
+        if query in t["name"].lower():
+            results.append(t)
+    return results
 
 def delete_teacher(teacher_id):
     for t in teachers:
